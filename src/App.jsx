3a75eceec49014cb01e1d34887faca5450f0de97
xoyn222 <kostuk186@gmail.com>
2024-09-13 01:26:25 +0300

    Sidebar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "./comp/Sidebar/Sidebar.jsx";
+
+const App = () => {
+  return(
+      <>
+          <Sidebar/>
+      </>
+  )
+}
+
+export default App
